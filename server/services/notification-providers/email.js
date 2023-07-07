c59cfbaf84c820753ad643f3df66cdb1ff36aeff
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2023-07-07 19:36:35 +0530

    Update email.js

--- server/services/notification-providers/email.js
+++ server/services/notification-providers/email.js
@@ -1,18 +1,19 @@
 'use strict'
+const formSubmissionTemplate = require('./email-templates/form-submission');
+
 module.exports = ({strapi}) => ({
   async send(config, formName, data) {
     let recipients = await strapi.query('plugin::ezforms.recipient').findMany()
     //Loop through data and construct message from data object
-    let formattedData = strapi.plugin('ezforms').service('formatData').formatData(data)
-    let message = formName !== "form" ? `${formName} \n ${formattedData}` : formattedData
+    let formattedTableData = strapi.plugin('ezforms').service('formatData').formatHtmlData(data)
     //loop through the recipients and send an email
     for (let recipient of recipients) {
       try {
         await strapi.plugins['email'].services.email.send({
           to: recipient.email,
           from: config.from,
-          subject: config.subject ? config.subject : 'New Form Submission',
-          text: message,
+          subject: config.subject ? config.subject : `New ${formName} Submission`,
+          html: formSubmissionTemplate(formattedTableData),
         })
       } catch (e) {
         strapi.log.error(e)
@@ -20,8 +21,4 @@ module.exports = ({strapi}) => ({
     }
 
   }
-
-
 })
-
-
