4235bc57af6a72803f1121b8dc656009d66d1be9
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2023-07-07 19:34:20 +0530

    Update formatData.js

--- server/services/utils/formatData.js
+++ server/services/utils/formatData.js
@@ -10,6 +10,17 @@ module.exports = ({strapi}) => ({
       }
     }
     return message
+  },
+  formatHtmlData(data) {
+    let message = ''
+    //Loop through data and construct message from data object
+    for (let key in data) {
+      if (typeof data[key] === 'object') {
+        message += `<tr><td style="padding: 10px 5px; width: 50vw; border: 1px solid #333333;">${key}</td><td style="width: 50vw; border: 1px solid #333333;"> ${JSON.stringify(data[key], null, 2)}</td></tr>`
+      } else {
+        message += `<tr><td style="padding: 10px 5px;width: 50vw; border: 1px solid #333333;">${key}</td><td style="width: 50vw; border: 1px solid #333333;"> ${data[key]}</td></tr>`
+      }
+    }
+    return message
   }
 })
-
